2a2564d75a1230a5955874691c4bc448bd87cd58
OxHazem <omarhazemhassan@gmail.com>
2025-12-10 09:43:57 +0200

    Adding test _rrandom_map

--- genrate_map/Test_random_map.py
+++ genrate_map/Test_random_map.py
@@ -1,5 +1,8 @@
-from genrate_map.random_map_generator import Random_map , is_reachable , print_grid
-grid ,start , objective ,drop_point = Random_map(10 ,0.2)
+from random_map_generator import Random_map , is_reachable , print_grid
+from genrated_map_phase2 import Random_map_for_multiple_bots
+grid, start_A, start_B, objective, drop_point = Random_map_for_multiple_bots(10 , 0.2)
 print_grid(grid)
+# grid ,start , objective ,drop_point = Random_map(10 ,0.2)
+# print_grid(grid)
 
 
